b0a17b5c3dfa6749a6b699ff14b526c4a24265ea
Mykhailo Shatilov <mykhailo@springimport.com>
2016-10-18 13:35:49 -0400

    new formatting

--- Block/Widget/CategoryWidget.php
+++ Block/Widget/CategoryWidget.php
@@ -1,20 +1,23 @@
 <?php
+
 namespace Emizentech\CategoryWidget\Block\Widget;
 
 class CategoryWidget extends \Magento\Framework\View\Element\Template implements \Magento\Widget\Block\BlockInterface
 {
-	protected $_template = 'widget/categorywidget.phtml';
-	 
-	    /**
+
+    protected $_template = 'widget/categorywidget.phtml';
+
+    /**
      * Default value for products count that will be shown
      */
-     const DEFAULT_IMAGE_WIDTH = 250;
-     const DEFAULT_IMAGE_HEIGHT = 250;
-         protected $_categoryHelper;
-     protected $categoryFlatConfig;
+    const DEFAULT_IMAGE_WIDTH = 250;
+    const DEFAULT_IMAGE_HEIGHT = 250;
 
-     protected $topMenu;
-     protected $_categoryFactory;
+    protected $_categoryHelper;
+    protected $categoryFlatConfig;
+    protected $topMenu;
+    protected $_categoryFactory;
+
     /**
      * @param \Magento\Framework\View\Element\Template\Context $context
      * @param \Magento\Catalog\Helper\Category $categoryHelper
@@ -26,13 +29,15 @@ class CategoryWidget extends \Magento\Framework\View\Element\Template implements
         \Magento\Catalog\Model\Indexer\Category\Flat\State $categoryFlatState,
         \Magento\Catalog\Model\CategoryFactory $categoryFactory,
         \Magento\Theme\Block\Html\Topmenu $topMenu
-    ) {
+    )
+    {
         $this->_categoryHelper = $categoryHelper;
         $this->categoryFlatConfig = $categoryFlatState;
         $this->topMenu = $topMenu;
         $this->_categoryFactory = $categoryFactory;
         parent::__construct($context);
     }
+
     /**
      * Return categories helper
      */
@@ -40,13 +45,14 @@ class CategoryWidget extends \Magento\Framework\View\Element\Template implements
     {
         return $this->_categoryHelper;
     }
-    
-     public function getCategorymodel($id)
+
+    public function getCategorymodel($id)
     {
-         $_category = $this->_categoryFactory->create();
-            $_category->load($id);
-            return $_category;
+        $_category = $this->_categoryFactory->create();
+        $_category->load($id);
+        return $_category;
     }
+
     /**
      * Retrieve current store categories
      *
@@ -55,66 +61,68 @@ class CategoryWidget extends \Magento\Framework\View\Element\Template implements
      * @param bool $toLoad
      * @return \Magento\Framework\Data\Tree\Node\Collection|\Magento\Catalog\Model\Resource\Category\Collection|array
      */
-   public function getCategoryCollection()
+    public function getCategoryCollection()
     {
         $category = $this->_categoryFactory->create();
-	if($this->getData('parentcat') > 0){
-		$rootCat = $this->getData('parentcat');
-        	$category->load($rootCat);
-	}
-	
-	if(!$category->getId()){
-        	$rootCat = $this->_storeManager->getStore()->getRootCategoryId();
-        	$category->load($rootCat);
-	}
+        if ($this->getData('parentcat') > 0) {
+            $rootCat = $this->getData('parentcat');
+            $category->load($rootCat);
+        }
+
+        if (!$category->getId()) {
+            $rootCat = $this->_storeManager->getStore()->getRootCategoryId();
+            $category->load($rootCat);
+        }
         $storecats = $category->getChildrenCategories();
         $storecats->addAttributeToSelect('image');
         return $storecats;
         // $catCollection = $this->_categoryHelper->getStoreCategories($sorted , $asCollection, $toLoad);
         // return $catCollection;
     }
-    
-    
+
     /**
      * Retrieve child store categories
      *
      */
     public function getChildCategories($category)
     {
-           if ($this->categoryFlatConfig->isFlatEnabled() && $category->getUseFlatResource()) {
-                $subcategories = (array)$category->getChildrenNodes();
-            } else {
-                $subcategories = $category->getChildren();
-            }
-            return $subcategories;
+        if ($this->categoryFlatConfig->isFlatEnabled() && $category->getUseFlatResource()) {
+            $subcategories = (array) $category->getChildrenNodes();
+        } else {
+            $subcategories = $category->getChildren();
+        }
+        return $subcategories;
     }
 
-   
-     /**
+    /**
      * Get the widht of product image
      * @return int
      */
-    public function getImagewidth() {
-		if($this->getData('imagewidth')==''){
-			return DEFAULT_IMAGE_WIDTH;
-		}
+    public function getImagewidth()
+    {
+        if ($this->getData('imagewidth') == '') {
+            return DEFAULT_IMAGE_WIDTH;
+        }
         return (int) $this->getData('imagewidth');
     }
-     /**
+
+    /**
      * Get the height of product image
      * @return int
      */
-    public function getImageheight() {
-		if($this->getData('imageheight')==''){
-			return DEFAULT_IMAGE_HEIGHT;
-		}
+    public function getImageheight()
+    {
+        if ($this->getData('imageheight') == '') {
+            return DEFAULT_IMAGE_HEIGHT;
+        }
         return (int) $this->getData('imageheight');
     }
-    
-    public function canShowImage(){
-    	if($this->getData('image') == 'image')
-    		return true;
-    	elseif($this->getData('image') == 'no-image')
-    	return false;
+
+    public function canShowImage()
+    {
+        if ($this->getData('image') == 'image')
+            return true;
+        elseif ($this->getData('image') == 'no-image')
+            return false;
     }
 }
