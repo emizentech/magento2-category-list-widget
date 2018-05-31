133ba96d8a44834a21bccaf7971a07f07728faaa
livz <steven@livz.org>
2018-05-31 08:34:33 +0100

    Allow specified sub-categories to be linked to a page rather than the category.

--- Block/Widget/CategoryWidget.php
+++ Block/Widget/CategoryWidget.php
@@ -1,52 +1,55 @@
 <?php
+
 namespace Emizentech\CategoryWidget\Block\Widget;
 
 class CategoryWidget extends \Magento\Framework\View\Element\Template implements \Magento\Widget\Block\BlockInterface
 {
     protected $_template = 'widget/categorywidget.phtml';
     protected $rootCategoryObject;
+    protected $pageCategories;
     protected $layerResolver;
 
-    const DEFAULT_IMAGE_WIDTH = 250;
+    const DEFAULT_IMAGE_WIDTH  = 250;
     const DEFAULT_IMAGE_HEIGHT = 250;
-    
+    const DEFAULT_DISPLAY_STYLE = 'image-and-text';
+
     /**
-    * \Magento\Catalog\Model\CategoryFactory $categoryFactory
-    */
+     * \Magento\Catalog\Model\CategoryFactory $categoryFactory.
+     */
     protected $_categoryFactory;
-    
+
     /**
-    * @param \Magento\Framework\View\Element\Template\Context $context
-    * @param \Magento\Catalog\Model\CategoryFactory $categoryFactory
-    * @param array $data
-    */
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Catalog\Model\CategoryFactory           $categoryFactory
+     * @param array                                            $data
+     */
     public function __construct(
     \Magento\Framework\View\Element\Template\Context $context,
     \Magento\Catalog\Model\CategoryFactory $categoryFactory,
     \Magento\Catalog\Model\Layer\Resolver $layerResolver
     ) {
         $this->_categoryFactory = $categoryFactory;
-        $this->layerResolver = $layerResolver;
-
         parent::__construct($context);
+
+        $this->layerResolver = $layerResolver;
     }
 
     /**
-    * Retrieve current store categories
-    *
-    * @return \Magento\Framework\Data\Tree\Node\Collection|\Magento\Catalog\Model\Resource\Category\Collection|array
-    */
+     * Retrieve current store categories.
+     *
+     * @return \Magento\Framework\Data\Tree\Node\Collection|\Magento\Catalog\Model\Resource\Category\Collection|array
+     */
     public function getCategoryCollection()
     {
         $category = $this->getRootCategory();
-         
+        
         $childCategories = $category->getChildrenCategories();
- 
+
         $childCategories->addAttributeToSelect('image');
- 
+
         return $childCategories;
     }
-    
+
     public function getRootCategory() {
         if(isset($this->rootCategoryObject)) {
             return $this->rootCategoryObject;
@@ -66,11 +69,64 @@ class CategoryWidget extends \Magento\Framework\View\Element\Template implements
         return $category;
     }
 
+    public function categoryIsPage($id) {
+
+        if (!isset($pageCategories)) {
+            $flatcats = $this->getData('categoriesaspage');
+            
+            if (!empty($flatcats)) {
+                $pageCategories = explode(',',$flatcats);
+            }
+        }
+
+        if (!empty($pageCategories)) {
+            return in_array($id, $pageCategories);
+        }
+
+        return false;
+    }
+
     public function getCurrentCategory()
     {
         return $this->layerResolver->get()->getCurrentCategory();
-    }    
-        
+    }
+
+    /**
+     * Get the width of product image.
+     *
+     * @return int
+     */
+    public function getImageWidth()
+    {
+        if ( empty($this->getData('imagewidth')) ) {
+            return self::DEFAULT_IMAGE_WIDTH;
+        }
+
+        return (int) $this->getData('imagewidth');
+    }
+
+    /**
+     * Get the height of product image.
+     *
+     * @return int
+     */
+    public function getImageHeight()
+    {
+        if ( empty($this->getData('imageheight')) ) {
+            return self::DEFAULT_IMAGE_HEIGHT;
+        }
+
+        return (int) $this->getData('imageheight');
+    }
+
+    public function getDisplayStyle() {
+        if(empty($this->getData('image'))) {
+            return self::DEFAULT_DISPLAY_STYLE;
+        }
+
+        return $this->getData('image');
+    }
+
     public function getTitle() {
         if(empty($this->getData('show_header'))) {
             return false;
@@ -83,37 +139,13 @@ class CategoryWidget extends \Magento\Framework\View\Element\Template implements
         return $this->getRootCategory()->getName();
     }
 
-    /**
-    * Get the width of product image
-    * @return int
-    */
-    public function getImageWidth() {
-        if ( empty($this->getData('imagewidth')) ) {
-            return self::DEFAULT_IMAGE_WIDTH;
-        }
-        return (int) $this->getData('imagewidth');
-    }
-
-    /**
-    * Get the height of product image
-    * @return int
-    */
-    public function getImageHeight() {
-        if ( empty($this->getData('imageheight')) ) {
-            return self::DEFAULT_IMAGE_WIDTH;
-        }
-        return (int) $this->getData('imageheight');
-    }
-    
-    public function canShowImage(){
-        if($this->getData('image') == 'image')
-            return true;
-        elseif($this->getData('image') == 'no-image')
-            return false;
+    public function canShowImage()
+    {
+        return in_array($this->getDisplayStyle(), ['image','image-and-text']);
     }
 
     public function canShowCategoryName()
     {
         return in_array($this->getDisplayStyle(), ['no-image','image-and-text']);
-    }    
+    }
 }
--- README.md
+++ README.md
@@ -6,6 +6,20 @@
 * Size of images can be adjusted from the widget settings in the admin panel.
 * By default, shows the child categories under the store's root category
 * Can be configured to display any category's children
+* Sub-Categories can be rendered as pages.
+
+# Usage
+Most of the functionalty is self-evident in Widget Options page when inserting the widget. Below is any further useful tips.
+
+### Parent Category Id (optional)
+Set this to the Id of the catergory whose children you want to list. This id can be found at the top of the Category management page.
+
+### Category as Page (optional)
+By default, the list of categories rendered link to a category page that shows the products within that category. Sometimes I can be useful to link to a page that shows the sub-categories rather than the products (e.g. The "pizzas" category shows a list of pizza categories rather than the pizzas themselves).
+
+Adding a comma separated list of ant category id's that should be rendered as a page will create a link to a page name that is the same as the category name - you should then create that page in the CMS and you can add another widget that renders the products themselves (using the category id as above).
+
+# Installation
 
 ## Composer Installation Instructions
 First you must add the GIT Repository to composer by running this command
