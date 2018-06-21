ff278165876f9d0ed4cda0193459c4e86820ff6a
Borut Kovačec <borut.kovacec@gmail.com>
2018-06-21 18:41:23 +0300

    Added Observer to add images to child categories collection

---
+++ Observer/AddImage.php
@@ -0,0 +1,18 @@
+<?php
+namespace Emizentech\CategoryWidget\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class AddImage implements ObserverInterface
+{
+    /**
+     * @param Observer $observer
+     * @return $this
+     */
+    public function execute(Observer $observer)
+    {
+        $select = $observer->getSelect();
+        return $select->columns('image');
+    }
+}
